57b148a204f4ca45aa37c16ad969a76a1cb3394f
Jakob Hansen <jakob@soundwall.com>
2018-01-10 12:20:12 -0700

    whitespace fix

--- resources/js/calendar-functions.js
+++ resources/js/calendar-functions.js
@@ -1,90 +1,89 @@
 $(function($) {
-	$.calendarUtils = {};
-	$.calendarUtils.gatherFormData = function () {
-	    var formData = new Object();
-	    formData.Title = $("#title").val();
-		formData.Description = $("#description").val();
-		formData.StartDate = $("#starttime").data("DateTimePicker").date().format("YYYY-MM-DD[T]HH:mm:ss");
-		formData.EndDate = $("#endtime").data("DateTimePicker").date().format("YYYY-MM-DD[T]HH:mm:ss");
-		formData.Id = $("#event-form").data("id");
-	    return formData;
-	}
+    $.calendarUtils = {};
+    $.calendarUtils.gatherFormData = function () {
+        var formData = new Object();
+        formData.Title = $("#title").val();
+        formData.Description = $("#description").val();
+        formData.StartDate = $("#starttime").data("DateTimePicker").date().format("YYYY-MM-DD[T]HH:mm:ss");
+        formData.EndDate = $("#endtime").data("DateTimePicker").date().format("YYYY-MM-DD[T]HH:mm:ss");
+        formData.Id = $("#event-form").data("id");
+        return formData;
+    }
 
-	$.calendarUtils.submitFormData = function(formData, success_callback = $.noop, error_callback = $.noop) {
-	    var method = (formData.Id == 0) ? "POST" : "PUT";
-	    $.ajax({
-	        type: method,
-	        url: "api/events_proxy",
-	        dataType: "json",
-	        data: JSON.stringify(formData),
-	        success: success_callback,
-	        error: error_callback
-	    });
-	}
+    $.calendarUtils.submitFormData = function(formData, success_callback = $.noop, error_callback = $.noop) {
+        var method = (formData.Id == 0) ? "POST" : "PUT";
+        $.ajax({
+            type: method,
+            url: "api/events_proxy",
+            dataType: "json",
+            data: JSON.stringify(formData),
+            success: success_callback,
+            error: error_callback
+        });
+    }
 
-	$.calendarUtils.deleteEvent = function (eventId, success_callback = $.noop, error_callback = $.noop) {
-	    $.ajax({
-	        type: "DELETE",
-	        url: "api/events_proxy/" + eventId,
-	        success: success_callback,
-	        error: error_callback
-	    });
-	}
+    $.calendarUtils.deleteEvent = function (eventId, success_callback = $.noop, error_callback = $.noop) {
+        $.ajax({
+            type: "DELETE",
+            url: "api/events_proxy/" + eventId,
+            success: success_callback,
+            error: error_callback
+        });
+    }
 
-	$.calendarUtils.populateFormData = function (id, title, description, start, end) {
-	    $("#event-form").data("id", id);
-	
-	    if (title !== "") {
-	        $("#title").val(title);
-	    } else {
-	        $("#title").val("");
-	        $("#title").attr("placeholder", "My New Event");
-	    }
+    $.calendarUtils.populateFormData = function (id, title, description, start, end) {
+        $("#event-form").data("id", id);
 
-	    if (description !== "") {
-	        $("#description").val(description);
-	    } else {
-	        $("#description").val("");
-	        $("#description").attr("placeholder", "Event Description");
-	    }
+        if (title !== "") {
+            $("#title").val(title);
+        } else {
+            $("#title").val("");
+            $("#title").attr("placeholder", "My New Event");
+        }
 
-	    $("#starttime").data("DateTimePicker").date(start.local().startOf("minute"));
+        if (description !== "") {
+            $("#description").val(description);
+        } else {
+            $("#description").val("");
+            $("#description").attr("placeholder", "Event Description");
+        }
 
-	    // if we're missing the end argument, we'll set it for 30 minutes                
-	    if (typeof(end) === "undefined") {
-	        var new_end = moment(start);
-	        new_end.add(30, "minutes");
-	    } else {
-	        var new_end = moment(end);
-	    }
+        $("#starttime").data("DateTimePicker").date(start.local().startOf("minute"));
 
-	    $("#endtime").data("DateTimePicker").date(new_end.local().startOf("minute"));
-	}
+        // if we're missing the end argument, we'll set it for 30 minutes
+        if (typeof(end) === "undefined") {
+            var new_end = moment(start);
+            new_end.add(30, "minutes");
+        } else {
+            var new_end = moment(end);
+        }
 
-	$.calendarUtils.setDialogMeta = function (dialog_title, save_text, needs_delete = false, cal_selector = $()) {
-	    console.debug(cal_selector);
-	    $("#dialog-title").text(dialog_title);
-	    $("#save_change").text(save_text);
-	    if (needs_delete) {
-	        if ($("#delete_button").length == 0) {
-	            $("<button type='button' class='btn btn-warning' id='delete_button'>Delete Event</button>").insertAfter("#close_button");
-	        }
-	    } else {
-	        if ($("#delete_button").length != 0) {
-	            $("#delete_button").remove();
-	        }
-	    }
-	    $("#event-dialog").data("cal_selector", cal_selector);
-	}
+        $("#endtime").data("DateTimePicker").date(new_end.local().startOf("minute"));
+    }
 
-	$.calendarUtils.closeModalAndRefetch = function () {
-		$("#event-dialog").modal("hide");
+    $.calendarUtils.setDialogMeta = function (dialog_title, save_text, needs_delete = false, cal_selector = $()) {
+        $("#dialog-title").text(dialog_title);
+        $("#save_change").text(save_text);
+        if (needs_delete) {
+            if ($("#delete_button").length == 0) {
+                $("<button type='button' class='btn btn-warning' id='delete_button'>Delete Event</button>").insertAfter("#close_button");
+            }
+        } else {
+            if ($("#delete_button").length != 0) {
+                $("#delete_button").remove();
+            }
+        }
+        $("#event-dialog").data("cal_selector", cal_selector);
+    }
+
+    $.calendarUtils.closeModalAndRefetch = function () {
+        $("#event-dialog").modal("hide");
         $("#calendar").fullCalendar('refetchEvents');
-	}
+    }
 
-	$.calendarUtils.formAlert = function (title, text) {
-		$("#error-alert .modal-title").text(title);
-		$("#error-alert .modal-body").html("<p>" + text + "</p>");
-		$("#error-alert").modal("show");
-	}
+    $.calendarUtils.formAlert = function (title, text) {
+        $("#error-alert .modal-title").text(title);
+        $("#error-alert .modal-body").html("<p>" + text + "</p>");
+        $("#error-alert").modal("show");
+    }
 }(jQuery));
--- resources/js/calendar-setup.js
+++ resources/js/calendar-setup.js
@@ -1,124 +1,124 @@
 $(document).ready(function () {
-	// set up the spinner whenever ajax requests are ongoing
-	$(document).ajaxStart(function(){
-	    $.LoadingOverlay("show");
-	});
+    // set up the spinner whenever ajax requests are ongoing
+    $(document).ajaxStart(function(){
+        $.LoadingOverlay("show");
+    });
 
-	$(document).ajaxStop(function(){
-	    $.LoadingOverlay("hide");
-	});
-	
-	$("#starttime").datetimepicker({
-	    stepping : 30,
-	    sideBySide : true
-	});
+    $(document).ajaxStop(function(){
+        $.LoadingOverlay("hide");
+    });
 
-	$("#endtime").datetimepicker({
-	    stepping : 30,
-	    sideBySide : true
-	});
+    $("#starttime").datetimepicker({
+        stepping : 30,
+        sideBySide : true
+    });
 
-	$("#event-dialog").on("hidden.bs.modal", function (e) {
-	    var cal_selector = $(this).data("cal_selector");
-	    cal_selector.css('background-color', 'white');
-	});
+    $("#endtime").datetimepicker({
+        stepping : 30,
+        sideBySide : true
+    });
 
-	$("#error-alert").on("show.bs.modal", function () {
-	    $("#event-dialog").css({"opacity": 0, "z-index":0});
-	});
-	 
-	$("#error-alert").on("hidden.bs.modal", function () {
-	    $("#event-dialog").css({"opacity": 1, "z-index" : 1050});
-	});
+    $("#event-dialog").on("hidden.bs.modal", function (e) {
+        var cal_selector = $(this).data("cal_selector");
+        cal_selector.css('background-color', 'white');
+    });
 
-	$("#calendar").fullCalendar({
-	    header : {
-	        left: "prev,next today",
-	        center: "title",
-	        right: "month,agendaWeek,agendaDay"
-	    },
-	    selectable: true,
-	    editable: true,
-	    eventDrop: function(event, delta, revertFunc) {
-	    	$.calendarUtils.populateFormData(event.id, event.title, event.description, event.start, event.end);
-	        var formData = $.calendarUtils.gatherFormData();
-	        $.calendarUtils.submitFormData(formData, $.calendarUtils.closeModalAndRefetch, revertFunc);
-	    },
-	    eventResize: function(event, delta, revertFunc) {
-	    	$.calendarUtils.populateFormData(event.id, event.title, event.description, event.start, event.end);
-	        var formData = $.calendarUtils.gatherFormData();
-	        $.calendarUtils.submitFormData(formData, $.calendarUtils.closeModalAndRefetch, revertFunc);
-	    },
-	    events: function(start, end, timezone, callback) {
-	        $.ajax({
-	            type: "GET",
-	            url: "api/events_proxy",
-	            dataType: "json",
-	            success: function(data) {
-	                var events = [];
-	                $.each(data, function(index, ev) {
-	                    events.push({
-	                        title: ev["Title"],
-	                        description: ev["Description"],
-	                        start: ev["StartDate"],
-	                        end: ev["EndDate"],
-	                        id: ev["Id"]
-	                    });
-	                });
-	                callback(events);
-	            },
-	            error: function (jqXHR, textStatus, errorThrown) {
-	            	$.calendarUtils.formAlert("Server Error!", "Unable to connect to calendar backend...");
-	            }
-	        });
-	    },
-	    selectAllow : function(selectInfo) { // we only consider it a 'select' event if it spans more than one day
-	        var diff = selectInfo.end.diff(selectInfo.start, "days", true);
-	        return (diff > 1.0);
-	    },
-	    select: function(start, end, jsEvent, view) {
-	        console.debug('select event fired....Start is: ' + start.format('YYYY-MM-DD[T]HH:mm:ss') + ', end is: ' + end.format('YYYY-MM-DD[T]HH:mm:ss'));
-	        $(this).css('background-color', '#fcffcc');
-	        $.calendarUtils.populateFormData(0, "", "", start, end);
-	        $.calendarUtils.setDialogMeta("Create New Event", "Create Event", false, $(this));
-	        $("#event-dialog").modal( "show" );
-	    },
-	    dayClick: function(date, jsEvent, view) {
-	        $(this).css('background-color', '#fcffcc');
-	        var now = moment();
-	        // Set the time components to current time, this is how google calendar works anyway
-	        date.set({
-	            "hour" : now.get("hour"),
-	            "minute" : now.get("minute") 
-	        });
-	        $.calendarUtils.populateFormData(0, "", "", date);
-	        $.calendarUtils.setDialogMeta("Create New Event", "Create Event", false, $(this));
-	        $("#event-dialog").modal("show");
-	    },
-	    eventClick: function(calEvent, jsEvent, view) {
-	    	$.calendarUtils.populateFormData(calEvent.id, calEvent.title, calEvent.description, calEvent.start, calEvent.end)
-	        $.calendarUtils.setDialogMeta("Edit Event", "Save Changes", true);
-	        $("#event-dialog").modal("show");
-	    }
-	});
+    $("#error-alert").on("show.bs.modal", function () {
+        $("#event-dialog").css({"opacity": 0, "z-index":0});
+    });
 
-	$(".modal-footer").on("click", "button#save_change", function(event) {
-	    var formData = $.calendarUtils.gatherFormData();
-	    $.calendarUtils.submitFormData(formData, $.calendarUtils.closeModalAndRefetch, function(jqXHR, textStatus, errorThrown) {
-	    	$.calendarUtils.formAlert("Server Error!", "Unable to connect to calendar backend...");
-	    });
-	});
+    $("#error-alert").on("hidden.bs.modal", function () {
+        $("#event-dialog").css({"opacity": 1, "z-index" : 1050});
+    });
 
-	$(".modal-footer").on("click", "button#delete_button", function(event) {
-	    var formData = $.calendarUtils.gatherFormData();
-	    if (formData.Id != 0) {
-	    	$.calendarUtils.deleteEvent(formData.Id, $.calendarUtils.closeModalAndRefetch, function (jqXHR, textStatus, errorThrown) {
-	        	if (jqXHR.status != 200 && jqXHR.status != 204) {
-	        		$.calendarUtils.formAlert("Server Error!", "Unable to connect to calendar backend...");
-	            } else {    
-	                $.calendarUtils.closeModalAndRefetch();
-	            }
-	        });
-	    }
-	});
+    $("#calendar").fullCalendar({
+        header : {
+            left: "prev,next today",
+            center: "title",
+            right: "month,agendaWeek,agendaDay"
+        },
+        selectable: true,
+        editable: true,
+        eventDrop: function(event, delta, revertFunc) {
+            $.calendarUtils.populateFormData(event.id, event.title, event.description, event.start, event.end);
+            var formData = $.calendarUtils.gatherFormData();
+            $.calendarUtils.submitFormData(formData, $.calendarUtils.closeModalAndRefetch, revertFunc);
+        },
+        eventResize: function(event, delta, revertFunc) {
+            $.calendarUtils.populateFormData(event.id, event.title, event.description, event.start, event.end);
+            var formData = $.calendarUtils.gatherFormData();
+            $.calendarUtils.submitFormData(formData, $.calendarUtils.closeModalAndRefetch, revertFunc);
+        },
+        events: function(start, end, timezone, callback) {
+            $.ajax({
+                type: "GET",
+                url: "api/events_proxy",
+                dataType: "json",
+                success: function(data) {
+                    var events = [];
+                    $.each(data, function(index, ev) {
+                        events.push({
+                            title: ev["Title"],
+                            description: ev["Description"],
+                            start: ev["StartDate"],
+                            end: ev["EndDate"],
+                            id: ev["Id"]
+                        });
+                    });
+                    callback(events);
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    $.calendarUtils.formAlert("Server Error!", "Unable to connect to calendar backend...");
+                }
+            });
+        },
+        selectAllow : function(selectInfo) { // we only consider it a 'select' event if it spans more than one day
+            var diff = selectInfo.end.diff(selectInfo.start, "days", true);
+            return (diff > 1.0);
+        },
+        select: function(start, end, jsEvent, view) {
+            console.debug('select event fired....Start is: ' + start.format('YYYY-MM-DD[T]HH:mm:ss') + ', end is: ' + end.format('YYYY-MM-DD[T]HH:mm:ss'));
+            $(this).css('background-color', '#fcffcc');
+            $.calendarUtils.populateFormData(0, "", "", start, end);
+            $.calendarUtils.setDialogMeta("Create New Event", "Create Event", false, $(this));
+            $("#event-dialog").modal( "show" );
+        },
+        dayClick: function(date, jsEvent, view) {
+            $(this).css('background-color', '#fcffcc');
+            var now = moment();
+            // Set the time components to current time, this is how google calendar works anyway
+            date.set({
+                "hour" : now.get("hour"),
+                "minute" : now.get("minute")
+            });
+            $.calendarUtils.populateFormData(0, "", "", date);
+            $.calendarUtils.setDialogMeta("Create New Event", "Create Event", false, $(this));
+            $("#event-dialog").modal("show");
+        },
+        eventClick: function(calEvent, jsEvent, view) {
+            $.calendarUtils.populateFormData(calEvent.id, calEvent.title, calEvent.description, calEvent.start, calEvent.end)
+            $.calendarUtils.setDialogMeta("Edit Event", "Save Changes", true);
+            $("#event-dialog").modal("show");
+        }
+    });
+
+    $(".modal-footer").on("click", "button#save_change", function(event) {
+        var formData = $.calendarUtils.gatherFormData();
+        $.calendarUtils.submitFormData(formData, $.calendarUtils.closeModalAndRefetch, function(jqXHR, textStatus, errorThrown) {
+            $.calendarUtils.formAlert("Server Error!", "Unable to connect to calendar backend...");
+        });
+    });
+
+    $(".modal-footer").on("click", "button#delete_button", function(event) {
+        var formData = $.calendarUtils.gatherFormData();
+        if (formData.Id != 0) {
+            $.calendarUtils.deleteEvent(formData.Id, $.calendarUtils.closeModalAndRefetch, function (jqXHR, textStatus, errorThrown) {
+                if (jqXHR.status != 200 && jqXHR.status != 204) {
+                    $.calendarUtils.formAlert("Server Error!", "Unable to connect to calendar backend...");
+                } else {
+                    $.calendarUtils.closeModalAndRefetch();
+                }
+            });
+        }
+    });
 });
